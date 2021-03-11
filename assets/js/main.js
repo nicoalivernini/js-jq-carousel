95f396a3b79c05b8fb7dfe896f2f83c3890c5d79
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-11 17:36:14 +0100

    Aggiunta movimento immagini e pallini

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,25 +6,61 @@
 
 //Imposto il ready per accertarmi del corretto caricamento
 
-  //Selezionare pallini
-  var pallino = $('.nav > .fa-circle');
-
-  //Selezionare immagini
-  var immagine = $('.images > img');
-
-
 //Selezionare frecce
 var frecciaDx = $('.next > .fa-angle-right');
 var frecciaSx = $('.prev > .fa-angle-left');
 
-// Al click su freccia a dx cambia immagine
 
+
+// Al click su freccia a DESTRA cambia immagine
 frecciaDx.click(function(){
 
-if (immagine.hasClass('first')) {
-  immagine.next().addClass('active')
-} else {
+  //Selezionare pallini
+  var pallino = $('.nav > i.active');
 
-}
+  //Selezionare immagini
+  var immagine = $('.images img.active');
+
+  immagine.removeClass('active')
+  pallino.removeClass('active')
+  immagine.next('img').addClass('active')
+  pallino.next('i').addClass('active')
+
+//Passaggio ultima prima
+  if (immagine.hasClass('last')) {
+    $('img.first').addClass('active')
+  }
+
+  if (pallino.hasClass('last')) {
+    $('i.first').addClass('active')
+  }
+
+})
+
+
+
+//Al click su freccia SINISTRA torna indietro
+frecciaSx.click(function(){
+
+  //Selezionare pallini
+  var pallino = $('.nav > i.active');
+
+  //Selezionare immagini
+  var immagine = $('.images img.active');
+
+  immagine.removeClass('active')
+  pallino.removeClass('active')
+  immagine.prev('img').addClass('active')
+  pallino.prev('i').addClass('active')
+
+  //Passaggio prima ultima
+    if (immagine.hasClass('first')) {
+      $('img.first').removeClass('active')
+      $('img.last').addClass('active')
+    }
+
+    if (pallino.hasClass('first')) {
+      $('i.last').addClass('active')
+    }
 
 })
