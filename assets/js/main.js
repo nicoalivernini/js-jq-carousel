7d6f9e937068236fef4909be89c1a210a0c42aab
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-11 17:41:11 +0100

    Esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
-// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini
-// dello slider In oltre per scorrere le immagini permettere anche l’uso delle frecce
-// sinistra e destra della tastiera Utiliziamo una classe first e last per capire quali
-// sono la prima e ultima immagine dello slider Utilizziamo una classe active per aiutarci
-//  a capire quale è l’immagine attuale da visualizzare nello slider
-
 //Imposto il ready per accertarmi del corretto caricamento
 
 //Selezionare frecce
 var frecciaDx = $('.next > .fa-angle-right');
 var frecciaSx = $('.prev > .fa-angle-left');
-
 
 
 // Al click su freccia a DESTRA cambia immagine
@@ -21,8 +14,10 @@ frecciaDx.click(function(){
   //Selezionare immagini
   var immagine = $('.images img.active');
 
+  //Rimuovo classe active
   immagine.removeClass('active')
   pallino.removeClass('active')
+  //Aggiungo all'elemento successivo la classe active
   immagine.next('img').addClass('active')
   pallino.next('i').addClass('active')
 
@@ -30,13 +25,10 @@ frecciaDx.click(function(){
   if (immagine.hasClass('last')) {
     $('img.first').addClass('active')
   }
-
   if (pallino.hasClass('last')) {
     $('i.first').addClass('active')
   }
-
 })
-
 
 
 //Al click su freccia SINISTRA torna indietro
@@ -48,8 +40,10 @@ frecciaSx.click(function(){
   //Selezionare immagini
   var immagine = $('.images img.active');
 
+  //Rimuovo classe active
   immagine.removeClass('active')
   pallino.removeClass('active')
+  //Aggiungo all'elemento successivo la classe active
   immagine.prev('img').addClass('active')
   pallino.prev('i').addClass('active')
 
@@ -58,9 +52,7 @@ frecciaSx.click(function(){
       $('img.first').removeClass('active')
       $('img.last').addClass('active')
     }
-
     if (pallino.hasClass('first')) {
       $('i.last').addClass('active')
     }
-
 })
